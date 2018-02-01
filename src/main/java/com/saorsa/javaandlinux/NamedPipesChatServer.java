d6d618c5616a9315409f1785683c64dee9b0612b
adautev <andrei@dautev.eu>
2018-02-01 15:45:42 +0200

    Started writing to client FIFO.

--- src/main/java/com/saorsa/javaandlinux/NamedPipesChatServer.java
+++ src/main/java/com/saorsa/javaandlinux/NamedPipesChatServer.java
@@ -10,6 +10,8 @@ public class NamedPipesChatServer {
 
     public static final String SERVER_PIPE = "/tmp/serverFIFO";
     public static final String CLIENT_PIPE_PATH = "/tmp/";
+    public static final String COMMAND_DELIMITER = ":#:";
+
     //I know this isn't the best use case! :D
     public static void main(String[] args ) throws IOException {
         CreateNamedPipeJNI pipe = new CreateNamedPipeJNI();
@@ -25,15 +27,34 @@ public class NamedPipesChatServer {
             while(true) {
                 String commandLine = serverPipe.readLine();
                 if(commandLine != null) {
-                    String[] commandParameters = commandLine.split(":#:");
+                    String[] commandParameters = commandLine.split(COMMAND_DELIMITER);
                     String fromUser = commandParameters[0];
                     String toUser = commandParameters[1];
                     String command = commandParameters[2];
                     String sentence = commandParameters[3];
-                    Thread.sleep(1000);
+
+                    String userPipePath = CLIENT_PIPE_PATH + toUser;
+                    File userPipeFile = new File(userPipePath);
+                    if(!userPipeFile.exists()) {
+                        pipe.create(userPipePath);
+                    }
+                    RandomAccessFile userPipe = new RandomAccessFile(
+                            userPipePath, "rw");
+                    switch(command) {
+                        case "send":
+                            userPipe.write(("receive" + COMMAND_DELIMITER + sentence).getBytes());
+                            break;
+                        default:
+                            break;
+
+                    }
+                    userPipe.close();
+
                     if (command == "quit") {
                         break;
                     }
+
+                    Thread.sleep(1000);
                 }
             }
             serverPipe.close();
