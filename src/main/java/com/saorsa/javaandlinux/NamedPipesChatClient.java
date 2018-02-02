a95e9112ab9868d7827a08994dad662eca466e43
adautev <andrei@dautev.eu>
2018-02-02 09:39:52 +0200

    Added a simple FIFO chat client. (:

--- src/main/java/com/saorsa/javaandlinux/NamedPipesChatClient.java
+++ src/main/java/com/saorsa/javaandlinux/NamedPipesChatClient.java
@@ -6,26 +6,34 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public class NamedPipesChatServer {
-    //echo "angel:#:andrey:#:send:#:Gazorpazorp" > /tmp/server
+public class NamedPipesChatClient {
+
     public static final String SERVER_PIPE = "/tmp/serverFIFO";
     public static final String CLIENT_PIPE_PATH = "/tmp/";
     public static final String COMMAND_DELIMITER = ":#:";
 
     //I know this isn't the best use case! :D
     public static void main(String[] args ) throws IOException {
-        CreateNamedPipeJNI pipe = new CreateNamedPipeJNI();
+        String userName = "";
+        String fifoPath = "";
+        if(args.length == 0 || args[0].length() < 2 ) {
+            System.out.println("Please specify username!");
+            System.exit(0);
+        }
+        userName = args[0];
+        fifoPath = CLIENT_PIPE_PATH + userName;
         try {
-            File serverPipeFile = new File(SERVER_PIPE);
-            if(!serverPipeFile.exists()) {
-                pipe.create(SERVER_PIPE);
+            CreateNamedPipeJNI pipe = new CreateNamedPipeJNI();
+            File clientPipeFile = new File(fifoPath);
+            if(!clientPipeFile.exists()) {
+                pipe.create(fifoPath);
             }
 
-            RandomAccessFile serverPipe = new RandomAccessFile(
-                    SERVER_PIPE, "r");
+            RandomAccessFile clientPipe = new RandomAccessFile(
+                    fifoPath, "r");
             //Yes, this will greatly benefit from pub/sub + multi threading. But who cares! :D
             while(true) {
-                String commandLine = serverPipe.readLine();
+                String commandLine = clientPipe.readLine();
                 if(commandLine != null) {
                     String[] commandParameters = commandLine.split(COMMAND_DELIMITER);
                     String fromUser = commandParameters[0];
@@ -33,31 +41,21 @@ public class NamedPipesChatServer {
                     String command = commandParameters[2];
                     String sentence = commandParameters[3];
 
-                    String userPipePath = CLIENT_PIPE_PATH + toUser;
-                    File userPipeFile = new File(userPipePath);
-                    if(!userPipeFile.exists()) {
-                        pipe.create(userPipePath);
-                    }
-                    RandomAccessFile userPipe = new RandomAccessFile(
-                            userPipePath, "rw");
                     switch(command) {
-                        case "send":
-                            userPipe.write((fromUser + COMMAND_DELIMITER + toUser + COMMAND_DELIMITER + "receive" + COMMAND_DELIMITER + sentence).getBytes());
+                        case "receive":
+                            System.out.println("Received message from " + fromUser + ": " + sentence);
                             break;
+                        case "break":
+                            System.out.println("Exiting...");
+                            clientPipe.close();
                         default:
                             break;
-
-                    }
-                    userPipe.close();
-
-                    if (command == "quit") {
-                        break;
                     }
 
                     Thread.sleep(1000);
                 }
             }
-            serverPipe.close();
+
         } catch (Exception ex)
         {
             ex.printStackTrace();
